828a6595d72a8c4b82c065fb423ced8520e713dc
Groot <amalsivadas08@gmail.com>
2024-11-16 16:25:24 +0530

    added lot api

--- server/index.js
+++ server/index.js
@@ -6,13 +6,21 @@ const port = 5000
 const app = express();
 const billRoutes = require('./routes/bills.routes')
 const productRoutes = require('./routes/productInfo.routes')
+const lotRoutes=require("./routes/lot.routes")
 // const billItemsRoutes = require('./routes/billItems.routes')
 
 app.use(express.json())
 app.use(cors())
 app.use('/bills',billRoutes)
 app.use('/api/products',productRoutes)
+
+
+
 // app.use('/billItems',billItemsRoutes)
+// lot
+
+
+app.use("/api/v1/lot",lotRoutes)
 
 app.listen(port, () => {
     console.log("Server is Running on " + port)
