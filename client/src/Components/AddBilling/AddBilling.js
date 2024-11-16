2d11db92cd33acac384f53f9398de04e048c3028
aswin-web <ashvinachu097@gmail.com>
2024-11-16 16:28:25 +0530

    PRODUCT CONTROLLER FIXED

--- client/src/Components/AddBilling/AddBilling.js
+++ client/src/Components/AddBilling/AddBilling.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "../AddBilling/AddBilling.css";
 import Table from "react-bootstrap/esm/Table";
 import jsPDF from "jspdf";
@@ -27,6 +27,7 @@ const AddBilling = () => {
 
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
+  
   const handleScan = async (product_number) => {
     try {
       const response = await axios.get(`http://localhost:5000/api/products/getSerial/${bill_number}/${product_number}`);
@@ -40,26 +41,28 @@ const AddBilling = () => {
     }
   };
 
+
   return (
     <>
-      <div className="nav-color">
-        <div className="position">
+    <div className="nav-color"> 
+    <div className="position">
           <Link to="/navbar">
             <b style={{ cursor: "pointer", color: "white" }}> Products </b>
           </Link>
           <Link to="/billing">
             <b style={{ cursor: "pointer", color: "white" }}> Billing </b>
           </Link>
-        </div>
+        </div></div>
         <div className="back-tab">
+        <div id="page-to-pdf">
+          <h2> Bill Details</h2>
           <BarcodeReader onScan={handleScan} />
-          <div id="page-to-pdf">
-            <h2 className="bills"> Bill Details </h2>
-            <Table striped bordered hover className="add-tab">
+
+        <Table striped bordered hover className="add-tab">
               <thead>
                 <tr>
                   <th> S.No </th>
-                  <th><Checkbox {...label} style={{ color: "white" }} /> Product.No </th>
+                  <th><Checkbox {...label} style={{ color: "white" }} checked /> Product.No </th>
                   <th><Checkbox {...label} style={{ color: "white" }} />Before weight</th>
                   <th><Checkbox {...label} style={{ color: "white" }} />After weight</th>
                   <th><Checkbox {...label} style={{ color: "white" }} />Difference</th>
@@ -87,15 +90,134 @@ const AddBilling = () => {
                 )}
               </tbody>
             </Table>
-          </div>
-          <br />
-          <button className="pdf" onClick={exportPDF}>
+            </div>
+            <button className="pdf" onClick={exportPDF}>
             Export as PDF
           </button>
-        </div>
-      </div>
+          
+            </div>
+
     </>
   );
 };
 
 export default AddBilling;
+
+
+
+
+// import React, { useState,useEffect } from "react";
+// import "../AddBilling/AddBilling.css";
+// import Table from "react-bootstrap/esm/Table";
+// import jsPDF from "jspdf";
+// import html2canvas from "html2canvas";
+// import Checkbox from "@mui/material/Checkbox";
+// import { Link, useParams } from "react-router-dom";
+// import BarcodeReader from 'react-barcode-reader';
+// import axios from "axios";
+
+// const AddBilling = () => {
+//   const [scannedProducts, setScannedProducts] = useState([]); 
+//   const { bill_number } = useParams();
+  
+//   const exportPDF = async () => {
+//     const input = document.getElementById("page-to-pdf");
+//     const canvas = await html2canvas(input);
+//     const imgData = canvas.toDataURL("image/png");
+
+//     const pdf = new jsPDF();
+//     const imgWidth = 190;
+//     const imgHeight = (canvas.height * imgWidth) / canvas.width;
+//     pdf.addImage(imgData, "PNG", 10, 10, imgWidth, imgHeight);
+//     pdf.save("billing_details.pdf");
+//   };
+
+//   const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+//   const handleScan = async (product_number) => {
+//     try {
+//       const response = await axios.get(`http://localhost:5000/api/products/getSerial/${bill_number}/${product_number}`);
+//       if (response.status === 200) {
+//         setScannedProducts(prevProducts => [...prevProducts, response.data.product]);  
+//       } else {
+//         console.error('Failed to fetch product');
+//       }
+//     } catch (error) {
+//       console.error('Error fetching product:', error);
+//     }
+//   };
+
+
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:5000/api/products/getAll');
+//         setScannedProducts(response.data);
+//       } catch (error) {
+//         console.error('Failed to fetch products:', error);
+//       }
+//     };
+//     fetchProducts();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="nav-color">
+//         <div className="position">
+//           <Link to="/navbar">
+//             <b style={{ cursor: "pointer", color: "white" }}> Products </b>
+//           </Link>
+//           <Link to="/billing">
+//             <b style={{ cursor: "pointer", color: "white" }}> Billing </b>
+//           </Link>
+//         </div>
+//         <div className="back-tab">
+//           <BarcodeReader onScan={handleScan} />
+//           <div id="page-to-pdf">
+//             <h2 className="bills"> Bill Details </h2>
+//             <Table striped bordered hover className="add-tab">
+//               <thead>
+//                 <tr>
+//                   <th> S.No </th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} /> Product.No </th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} />Before weight</th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} />After weight</th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} />Difference</th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} />Adjustment</th>
+//                   <th><Checkbox {...label} style={{ color: "white" }} />Final weight</th>
+//                 </tr>
+//               </thead>
+//               <tbody>
+//                 {scannedProducts.length > 0 ? (
+//                   scannedProducts.map((product, index) => (
+//                     <tr key={index}>
+//                       <td>{index + 1}</td>
+//                       <td>{product.product_number}</td>
+//                       <td>{product.before_weight}</td>
+//                       <td>{product.after_weight}</td>
+//                       <td>{product.difference}</td>
+//                       <td>{product.adjustment}</td>
+//                       <td>{product.final_weight}</td>
+//                     </tr>
+//                   ))
+//                 ) : (
+//                   <tr>
+//                     <td colSpan="7">No product scanned</td>
+//                   </tr>
+//                 )}
+//               </tbody>
+//             </Table>
+//           </div>
+//           <br />
+//           <button className="pdf" onClick={exportPDF}>
+//             Export as PDF
+//           </button>
+//         </div>
+//       </div>
+      
+//     </>
+//   );
+// };
+
+// export default AddBilling;   
--- server/controllers/productInfo_V1.controller.js
+++ server/controllers/productInfo_V1.controller.js
@@ -1,12 +1,11 @@
 // const express = require('express')
 // const { PrismaClient } = require('@prisma/client')
 // const prisma = new PrismaClient()
- 
- 
+
 // const getAllProducts = async (req, res) => {
 //     try {
 //         const getProducts = await prisma.product_info.findMany();
-//         res.status(200).json(getProducts);  
+//         res.status(200).json(getProducts);
 //     } catch (error) {
 //         console.log(error);
 //         res.status(404).json({ error: "No Products found" });
@@ -15,7 +14,7 @@
 // const getProductByNumber = async (req, res) => {
 //   try {
 //       const { product_number } = req.params;
-     
+
 //       const product = await prisma.product_info.findUnique({
 //           where: { product_number },
 //           select: {
@@ -31,62 +30,54 @@
 //               updated_at: true
 //           }
 //       });
- 
-     
+
 //       res.status(200).json({ message: "Product found", product });
 //   } catch (error) {
 //       console.log(error);
 //       res.status(500).json({ error: "An error occurred while fetching the product" });
 //   }
 // };
- 
+
 // const createNewProduct = async (req, res) => {
 //     try {
-//       const { tag_number, before_weight, after_weight, product_number } = req.body;  
- 
- 
+//       const { tag_number, before_weight, after_weight, product_number } = req.body;
+
 //       const weight1 = parseFloat(before_weight);
 //       const weight2 = parseFloat(after_weight);
-     
- 
+
 //       const weight3 = weight1 - weight2;
 //       const weight4 = weight3 - (weight3 * 0.0001);
 //       const weight5 = weight4 - (weight4 * 0.1);
- 
-   
+
 //       let finalProductNumber = product_number || '';
- 
+
 //       if (!finalProductNumber) {
-   
+
 //         const weight5Str = Math.abs(weight5).toFixed(3).replace('.', '').slice(0, 3);
- 
-     
+
 //         const latestProduct = await prisma.product_info.findFirst({
 //           orderBy: {
 //             created_at: "desc"
 //           }
 //         });
- 
-   
+
 //         if (latestProduct && latestProduct.product_number) {
 //           const lastSerialNumber = latestProduct.product_number;
 //           const lastLetter = lastSerialNumber.charAt(0);
- 
-       
+
 //           if (lastSerialNumber.endsWith('99')) {
-         
+
 //             finalProductNumber = `${String.fromCharCode(lastLetter.charCodeAt(0) + 1)}00${weight5Str}`;
 //           } else {
-         
+
 //             finalProductNumber = `${lastLetter}00${weight5Str}`;
 //           }
 //         } else {
-         
+
 //           finalProductNumber = `A00${weight5Str}`;
 //         }
 //       }
- 
-     
+
 //       const newProduct = await prisma.product_info.create({
 //         data: {
 //           tag_number,
@@ -100,23 +91,19 @@
 //           updated_at: new Date()
 //         }
 //       });
- 
-     
+
 //       res.status(200).json({ message: "Product Successfully Created", newProduct });
 //     } catch (error) {
 //       console.log(error);
 //       res.status(404).json({ error: "Error Creating Product" });
 //     }
 //   };
- 
- 
- 
- 
+
 // const UpdatingProduct = async (req, res) => {
 //     try {
 //         const { id } = req.params;
 //         const { tag_number,before_weight,after_weight,difference,adjustment,final_weight, product_number } = req.body;
- 
+
 //         const updateProduct = await prisma.product_info.update({
 //             where: { id: parseInt(id) },
 //             data: {
@@ -130,16 +117,14 @@
 //                 updated_at: new Date(),
 //             }
 //         });
- 
-     
+
 //         res.status(200).json({ message: "Updated Successfully", updateProduct });
 //     } catch (error) {
 //         console.log(error);
 //         res.status(404).json({ error: "Product not Updated" });
 //     }
 // };
- 
- 
+
 // const deleteProduct = async (req, res) => {
 //     try {
 //       const { id } = req.params;
@@ -152,9 +137,7 @@
 //       res.status(404).json({ error: "Product not deleted" });
 //     }
 //   };
- 
- 
- 
+
 // const deleteAllProduct = async (req, res) => {
 //     try {
 //         const deleteAllProducts = await prisma.product_info.deleteMany();
@@ -164,81 +147,192 @@
 //         res.status(404).json({ error: "Can't delete All Product" });
 //     }
 // };
- 
+
 // module.exports = { getAllProducts, createNewProduct,getProductByNumber, UpdatingProduct, deleteProduct,deleteAllProduct }
 
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-
-
-const express = require('express')
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
- 
- 
-const getAllProducts = async (req, res) => {
-    try {
-        const getProducts = await prisma.product_info.findMany();
-        res.status(200).json(getProducts);  
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ error: "No Products found" });
-    }
-}
- 
-const getProductByNumber = async (req, res) => {
+const createNewProduct = async (req, res) => {
   try {
-      const { bill_number,product_number } = req.params;
-     
-      const product = await prisma.product_info.findUnique({
-          where: { product_number },
-          select: {
-            id : true,
-              product_number: true,
-              before_weight: true,
-              after_weight: true,
-              difference: true,
-              adjustment: true,
-              final_weight: true,
-              tag_number: true,
-              created_at: true,
-              updated_at: true
-          }
-      });
- 
-      const billItem = await prisma.bill_items.create({
-        data : {
-          bill_number,
-          product_id : product.id||5,
-          created_at : new Date()
-        }
-      })
-     
-      res.status(200).json({
-        message: "Product found",
-        product,
-        
- 
-      });
+    const {
+      tag_number,
+      before_weight,
+      after_weight,
+      product_number,
+      lot_id,
+      barcode_weight,
+    } = req.body;
+
+    const weight1 = parseFloat(before_weight);
+    const weight2 = parseFloat(after_weight);
+
+    // const weight3 = parseFloat((weight1 - weight2).toFixed(3));
+    // const weight4 = parseFloat((weight3 - (weight3 * 0.0001)).toFixed(3));
+    // const weight5 = parseFloat((weight4 - (weight4 * 0.1)).toFixed(3));
+
+    // const weight5Str = "123"
+
+    // const finalProductNumber = `${tag_number}00${weight5Str}`;
+    const finalProductNumber = product_number;
+
+    const newProduct = await prisma.product_info.create({
+      data: {
+        tag_number,
+        before_weight: weight1,
+        after_weight: weight2,
+        // difference: weight3,
+        // adjustment: weight4,
+        // final_weight: weight5,
+        barcode_weight: parseFloat(barcode_weight),
+        product_number: tag_number + Math.random(4) * 1000,
+        lot_id: lot_id,
+      },
+    });
+
+    res.status(200).json({
+      message: "Product Successfully Created",
+      newProduct,
+    });
   } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "An error occurred while fetching the product" });
+    console.log(error);
+    res.status(404).json({ error: "Error Creating Product" });
   }
 };
- 
+
+const getAllProducts = async (req, res) => {
+  try {
+    const lot_id = parseInt(req.params.lot_id);
+    const getProducts = await prisma.product_info.findMany({
+      where: {
+        lot_id,
+      },
+    });
+    res.status(200).json(getProducts);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "No Products found" });
+  }
+};
+
+const getProductByNumber = async (req, res) => {
+  try {
+    const { bill_number, product_number, bill_type } = req.params;
+    const billing_type = bill_type === "party" ? "hold" : "sold";
+    const product = await prisma.product_info.findMany({
+      where: {
+        product_number,
+        product_type: "active",
+        lot_info: { lot_process: "completed" },
+      },
+      select: {
+        id: true,
+        product_number: true,
+        before_weight: true,
+        after_weight: true,
+        difference: true,
+        adjustment: true,
+        final_weight: true,
+        tag_number: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+    if (product.length === 0) {
+      return res.status(500).json({ msg: "Product not found" });
+    }
+    const updateProduct = await prisma.product_info.updateMany({
+      where: {
+        id: product[0].id,
+      },
+      data: {
+        product_type: billing_type,
+      },
+    });
+    const billItem = await prisma.bill_items.create({
+      data: {
+        bill_number,
+        product_id: product[0].id,
+        created_at: new Date(),
+      },
+    });
+
+    res.status(200).json({
+      message: "Product found",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the product" });
+  }
+};
+
+const restoreProductByNumber = async (req, res) => {
+  try {
+    const { product_number } = req.params;
+    const billing_type = "active";
+    const product = await prisma.product_info.updateMany({
+      where: {
+        product_number,
+      },
+      data: {
+        product_type: billing_type,
+      }
+      
+    });
+    // console.log(product)
+    if (product.length===0){
+      res
+      .status(500)
+      .json({ error: "Product Not found" });
+    
+    }
+    const product_info = await prisma.product_info.findMany({
+      where: {
+        product_number,
+      },
+      select: {
+        id: true,
+        product_number: true,
+        before_weight: true,
+        after_weight: true,
+        difference: true,
+        adjustment: true,
+        final_weight: true,
+        tag_number: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+    res.status(200).json({
+      message: "Product found",
+      product_info,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the product" });
+  }
+};
+
 // const createNewProduct = async (req, res) => {
 //     try {
-//       const { tag_number, before_weight, after_weight } = req.body;  
+//       const { tag_number, before_weight, after_weight } = req.body;
 //       const {bill_number} = req.params;
- 
+
 //       const weight1 = parseFloat(before_weight);
 //       const weight2 = parseFloat(after_weight);
- 
+
 //       const weight3 = (weight1 - weight2);
 //       const weight4 = weight3 - (weight3 * 0.0001);
 //       const weight5 = weight4 - (weight4 * 0.1);
 //       const weight5Str = Math.abs(weight5).toFixed(3).replace('.', '').slice(0, 3);
 //      const  finalProductNumber = `${tag_number}00${weight5Str}`
-   
+
 //               const newProduct = await prisma.product_info.create({
 //         data: {
 //           tag_number,
@@ -252,27 +346,23 @@ const getProductByNumber = async (req, res) => {
 //           updated_at: new Date()
 //         }
 //       });
- 
- 
+
 //       res.status(200).json({
 //         message: "Product Successfully Created",
 //         newProduct,
-   
+
 //       });
 //     } catch (error) {
 //       console.log(error);
 //       res.status(404).json({ error: "Error Creating Product" });
 //     }
 //   };
- 
- 
- 
- 
+
 // const UpdatingProduct = async (req, res) => {
 //     try {
 //         const { id } = req.params;
 //         const { tag_number,before_weight,after_weight,difference,adjustment,final_weight, product_number } = req.body;
- 
+
 //         const updateProduct = await prisma.product_info.update({
 //             where: { id: parseInt(id) },
 //             data: {
@@ -286,113 +376,67 @@ const getProductByNumber = async (req, res) => {
 //                 updated_at: new Date(),
 //             }
 //         });
- 
-     
+
 //         res.status(200).json({ message: "Updated Successfully", updateProduct });
 //     } catch (error) {
 //         console.log(error);
 //         res.status(404).json({ error: "Product not Updated" });
 //     }
 // };
- 
-
-
-
-const createNewProduct = async (req, res) => {
-  try {
-      const { tag_number, before_weight, after_weight,product_number } = req.body;
-      const { bill_number } = req.params;
-
-      console.log(req.body,"kkkkkkkkkkkkkkkkkkkkkkk")
-
-      const weight1 = parseFloat(before_weight);
-      const weight2 = parseFloat(after_weight);
-
-      const weight3 = parseFloat((weight1 - weight2).toFixed(3)); 
-      const weight4 = parseFloat((weight3 - (weight3 * 0.0001)).toFixed(3)); 
-      console.log(weight4,"ooooooooooooooooooo")
-      const weight5 = parseFloat((weight4 - (weight4 * 0.1)).toFixed(3)); 
-
-      console.log(weight5,"iiiiasdasdada")
-
-      const weight5Str = Math.abs(weight5).toFixed(3).replace('.', '').slice(0, 3);
-
-      console.log(weight5Str,"jadsfsagvdkagdskagjb")
-      // const finalProductNumber = `${tag_number}00${weight5Str}`;
-      const finalProductNumber=product_number
-
-      const newProduct = await prisma.product_info.create({
-          data: {
-              tag_number,
-              before_weight: weight1,
-              after_weight: weight2,
-              difference: weight3,
-              adjustment: weight4,
-              final_weight: weight5,
-              product_number: finalProductNumber,
-              created_at: new Date(),
-              updated_at: new Date()
-          }
-      });
-
-      res.status(200).json({
-          message: "Product Successfully Created",
-          newProduct,
-      });
-  } catch (error) {
-      console.log(error);
-      res.status(404).json({ error: "Error Creating Product" });
-  }
-};
 
 const UpdatingProduct = async (req, res) => {
   try {
-      const { id } = req.params;
-      const { tag_number, before_weight, after_weight, difference, adjustment, final_weight, product_number } = req.body;
+    const { id } = req.params;
+    const { before_weight, after_weight, barcode_weight } = req.body;
 
-      const updateProduct = await prisma.product_info.update({
-          where: { id: parseInt(id) },
-          data: {
-              tag_number,
-              before_weight,
-              after_weight,
-              difference: parseFloat(difference.toFixed(3)), 
-              adjustment: parseFloat(adjustment.toFixed(3)), 
-              final_weight: parseFloat(final_weight.toFixed(3)), 
-              product_number,
-              updated_at: new Date(),
-          }
-      });
+    const updateProduct = await prisma.product_info.update({
+      where: { id: parseInt(id) },
+      data: {
+        before_weight,
+        after_weight,
+        barcode_weight: parseFloat(barcode_weight),
+        updated_at: new Date(),
+      },
+    });
 
-      res.status(200).json({ message: "Updated Successfully", updateProduct });
+    res.status(200).json({ message: "Updated Successfully", updateProduct });
   } catch (error) {
-      console.log(error);
-      res.status(404).json({ error: "Product not Updated" });
+    console.log(error);
+    res.status(404).json({ error: "Product not Updated" });
   }
 };
- 
+
 const deleteProduct = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const delProduct = await prisma.product_info.delete({
-        where: { id: parseInt(id) },
-      });
-      res.status(200).json({ message: "Deleted Successfully", delProduct });
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({ error: "Product not deleted" });
-    }
-  };
- 
-  
-const deleteAllProduct = async (req, res) => {
-    try {
-        const deleteAllProducts = await prisma.product_info.deleteMany();
-        res.status(200).json({ message: "Deleted Successfully", deleteAllProducts });
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ error: "Can't delete All Product" });
-    }
+  try {
+    const { id } = req.params;
+    const delProduct = await prisma.product_info.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).json({ message: "Deleted Successfully", delProduct });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "Product not deleted" });
+  }
 };
- 
-module.exports = { getAllProducts,getProductByNumber, createNewProduct, UpdatingProduct, deleteProduct,deleteAllProduct }
+
+const deleteAllProduct = async (req, res) => {
+  try {
+    const deleteAllProducts = await prisma.product_info.deleteMany();
+    res
+      .status(200)
+      .json({ message: "Deleted Successfully", deleteAllProducts });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "Can't delete All Product" });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductByNumber,
+  createNewProduct,
+  UpdatingProduct,
+  deleteProduct,
+  deleteAllProduct,
+  restoreProductByNumber,
+};
